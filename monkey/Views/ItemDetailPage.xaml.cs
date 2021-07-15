f1e18a10cc4e4458e72c57c6bbc3a1507f182f03
Nicholas Ghobrial <nicholasghob@yahoo.com>
2021-07-15 00:48:02 -0400

    Initial template for app development

---
+++ monkey/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using monkey.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace monkey.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ monkey/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using monkey.Models;
+using monkey.ViewModels;
+using monkey.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace monkey.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
